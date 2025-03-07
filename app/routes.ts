ae64a05e1863462af4365e1572dcfcc4b4a0f9e2
satrujit11 <beherasatrujit12@gmail.com>
2025-03-07 14:53:53 +0530

    fix: done

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,5 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/test.tsx"),
-  route("dr", "routes/home.tsx"),
+  index("routes/home.tsx"),
 ] satisfies RouteConfig;
